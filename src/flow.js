4356d121054092735f401322564eead65663d703
Dolgih Natalia <n.e.dolgih@yandex.ru>
2016-03-09 23:22:16 +0500

    done

--- src/flow.js
+++ src/flow.js
@@ -1,15 +1,11 @@
 'use strict';
 
 function serial(func, cb) {
-    if (checkFunctionArray(func)) {
-        var index = 0;
-        if (func.length > 0) {
-            func[index](callback);
-        } else {
-            cb(null, null);
-        }
+    var index = 0;
+    if (func.length > 0) {
+        func[index](callback);
     } else {
-        cb(new Error('it\'s not an array of functions'))
+        cb(null, null);
     }
     function callback(err, data) {
         if (err) {
@@ -25,56 +21,56 @@ function serial(func, cb) {
     }
 }
 
-function checkFunctionArray(arr) {
-    return arr instanceof Array && arr.every(function (f) {
-        return f instanceof Function;
-    });
-}
-
 function parallelCallback(index, cb) {
     var _this = this;
     return function (err, data) {
         if (err) {
             cb(err);
-        }
-        _this[index] = {value: data};
-        if (full(_this)) {
-            cb(null, unpackObjects(_this));
+        } else {
+            _this[index] = {value: data};
+            if (full(_this)) {
+                cb(null, unpackObjects(_this));
+            }
         }
     };
 }
 
 function parallel(func, cb) {
-    if (checkFunctionArray(func)) {
+    if (func.length > 0) {
         var result = new Array(func.length);
         func.forEach(function (f, i) {
             f(parallelCallback.call(result, i, cb));
         });
     } else {
-        cb(null, null);
+        cb(null, []);
     }
+
 }
 
 function map(array, func, cb) {
-    var result = new Array(array.length);
-    array.forEach(function (item, i) {
-        func(item, parallelCallback.call(result, i, cb));
-    });
+    if (func.length > 0) {
+        var result = new Array(array.length);
+        array.forEach(function (item, i) {
+            func(item, parallelCallback.call(result, i, cb));
+        });
+    } else {
+        cb(null, []);
+    }
 }
 
 function full(array) {
     for (var i = 0; i < array.length; i++) {
         if (array[i] === undefined) {
+
             return false;
         }
     }
+
     return true;
 }
 
 function unpackObjects(array) {
-    return array.map(function (item) {
-        return item.value;
-    });
+    return array.map(item => item.value);
 }
 
 
--- test/flow-test.js
+++ test/flow-test.js
@@ -1,43 +1,37 @@
 const flow = require('../src/flow');
 const assert = require('assert');
-var describe = require('mocha').describe;
-var it = require('mocha').it;
-var sinon = require('sinon');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const sinon = require('sinon');
 
-
-describe('flow.serial tests', function ()  {
-    it('should not work with wrong input', function () {
-        flow.serial(['foo', 'bar'], function (err) {
-            assert(err instanceof Error, 'returned error');
-        });
-    });
+describe('flow.serial tests', function () {
     it('should call all functions once', function ()  {
-        var spy1 = sinon.spy(function (cb) {cb(null, 1);});
-        var spy2 = sinon.spy(function (x, cb) {cb(null, 1);});
-        var cb = function (err, x) {};
+        var spy1 = sinon.spy((cb) => {cb(null, 1);});
+        var spy2 = sinon.spy((x, cb) => {cb(null, 1);});
+        var cb = () => {};
         flow.serial([spy1, spy2], cb);
         assert(spy1.calledOnce, 'every function called once');
         assert(spy2.calledOnce, 'every function called once');
     });
-    it('should call functions with result of previous', function () {
-        var func1 = function (cb) {cb(null, 1);};
-        var func2 = function (x, cb) {cb(null, x+1);};
-        var cb = function (err, x) {
-            assert.equal(x, 2, 'result should be equal 2')
+    it('should call functions with result of previous', function (done) {
+        var func1 = (cb) => {setTimeout(cb, 100, null, 1)};
+        var func2 = (x, cb) => {cb(null, x+1);};
+        var cb = (err, x) => {
+            assert.equal(x, 2, 'result should be equal 2');
+            done();
         };
         flow.serial([func1, func2], cb);
     });
     it('should call a callback function if error occurs', function () {
-        var func = function (cb) {cb(1);};
-        var spy = sinon.spy(function (x, cb) {cb(null, 1);});
-        var cb = function (err) {
+        var func = (cb) => {cb(1);};
+        var spy = sinon.spy((x, cb) => {cb(null, 1);});
+        var cb = (err) => {
             assert.equal(err, 1, 'callback was not called')
         };
         flow.serial([func, spy], cb);
         assert.equal(spy.callCount, 0, 'other functions should not be called')
     });
     it('should return no error if there is no functions', function () {
-
         var cb = function (err) {
             assert.equal(err, null, 'callback was not called');
         };
@@ -46,8 +40,62 @@ describe('flow.serial tests', function ()  {
 });
 
 describe('flow.parallel tests', function ()  {
-    it('should ', function () {
-
+    it('should not shuffle results', function () {
+        var func1 = (cb) => {cb(null, 'a');};
+        var func2 = (cb) => {cb(null, 'b');};
+        var cb =  (err, x) => {
+            assert.deepEqual(x, ['a', 'b'], 'arrays should be equal')
+        };
+        flow.parallel([func1, func2], cb);
+    });
+    it('should call all functions once', function ()  {
+        var spy1 = sinon.spy((cb) => {cb(null, 1);});
+        var spy2 = sinon.spy((cb) => {cb(null, 1);});
+        var cb = () => {};
+        flow.parallel([spy1, spy2], cb);
+        assert(spy1.calledOnce, 'every function called once');
+        assert(spy2.calledOnce, 'every function called once');
+    });
+    it('should call a callback function if error occurs', function (done) {
+        var func = (cb) => {setTimeout(cb, 100, 1)};
+        var spy = sinon.spy((cb) => {cb(null, 1);});
+        var cb = (err, x) => {
+            assert.equal(err, 1, 'callback was not called');
+            assert.notEqual(x, 1, 'callback was not called');
+            done();
+        };
+        flow.parallel([func, spy], cb);
+    });
+    it('should return no error if there is no functions', function () {
+        var cb = (err, res) =>{
+            assert.equal(err, null, 'callback was called with error');
+            assert.deepEqual(res, [], 'expected empty array');
+        };
+        flow.parallel([], cb);
+    });
+    it('should return result when all functions return results', function (done) {
+        var f1 = (cb) => {setTimeout(cb, 100, null, 'f1')};
+        var f2 = (cb) => {cb(null, 'f2')};
+        flow.parallel([f1, f2], (err, res) => {
+            assert.deepEqual(res, ['f1', 'f2'], 'arrays not equal');
+            done();
+        })
     });
 });
+describe('flow.map tests', function ()  {
+    it('should not shuffle results', function () {
+        var f = (e, cb) =>{cb(null, e + ' a');};
+        var cb = (err, x) => {
+            assert.deepEqual(x, ['x a', 'y a'], 'arrays should be equal')
+        };
+        flow.map(['x', 'y'], f, cb);
+    });
+    it('should return result when all functions return results', function (done) {
+        var f = (e, cb) => {setTimeout(cb, Math.random() * 100, null, 'f' + e)};
+        flow.map(['a', 'b', 'c'], f, (err, res) => {
+            assert.deepEqual(res, ['fa', 'fb', 'fc'], 'arrays not equal');
+            done();
+        })
+    });
 
+});
